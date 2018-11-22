1362dc2c2e5042d97690d7fbf4be69f34534599c
Christian Knoll <chris@sectorfive.com>
2018-11-22 11:38:58 -0500

    Adding new movies works, then lists all movies

--- routes/movies.js
+++ routes/movies.js
@@ -9,7 +9,7 @@ router.get('/make', function (req, res, next) {
     .then(() => console.log('connected to the database'))
     .catch(err => console.error('error connecting to db: ', err.errmsg));
 
-  // Make two movies
+  // Make two example movies to populate the database
   var movie1 = new Movie({
     name: 'Alien',
     releaseDate: 1979,
@@ -23,20 +23,24 @@ router.get('/make', function (req, res, next) {
 
   movie1.save(function (err) {
     if (err) throw err;
-    console.log('Movie 1 created!');
   });
   movie2.save(function (err) {
     if (err) throw err;
-    console.log('Movie 2 created!');
   });
   res.status(200).send('Saved!');
 });
 
 // GET
 router.get('/', function (req, res, next) {
-  Movie.find()
+
+  mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true })
+    .then(() => console.log('connected to the database'))
+    .catch(err => console.error('error connecting to db: ', err.errmsg));
+
+  Movie
+    .find()
+    .sort('name')
     .then(movies => {
-      console.log('movies is ', movies);
       res.render('movies', { title: 'movie page', movies: movies });
     }).catch(err => {
       res.status(500).send({
@@ -52,8 +56,6 @@ router.post('/', function (req, res, next) {
     .then(() => console.log('connected to the database'))
     .catch(err => console.error('error connecting to db: ', err.errmsg));
 
-  console.log('req.body is ', req.body);
-
   // Validate request
   if (!req.body.name) {
     return res.status(400).send({
@@ -68,12 +70,10 @@ router.post('/', function (req, res, next) {
     votes: 0
   });
 
-  console.log('req.body is now ', req.body);
-
   // Save Movie in the database
   movie.save()
-    .then(data => {
-      res.status(200).send(data);
+    .then(movies => {
+      res.status(200).redirect('movies');
     }).catch(err => {
       res.status(500).send({
         message: err.message || "Some error occurred while creating the Note."
